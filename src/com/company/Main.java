ef21d0708dd12c989b897dfd7f97012a0efa0daf
Zakiyah Walker <zakiyahwalker@Zakiyahs-MBP.fios-router.home>
2020-08-31 16:22:44 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int testscr = 50;
+    float prc = 24.99f;
+    String n = "Bobby Johnson";
+    System.out.println("Test Score initialized is: " + testscr);
+    System.out.println("Price tag: " + prc);
+    System.out.println("Username: " + n);
+    }
+}
